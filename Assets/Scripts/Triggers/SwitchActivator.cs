5503f2a0ef8f5179984c1754c8f1a692aca5bc0f
Hawas-Adel <Hawas-Adel@users.noreply.github.com>
2020-09-10 08:41:48 +0200

    Added Interaction range limit to switch activator

--- Assets/Scripts/Triggers/SwitchActivator.cs
+++ Assets/Scripts/Triggers/SwitchActivator.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 [ExecuteAfter(typeof(MouseHoverMonitor))]
 public class SwitchActivator : MonoBehaviour
 {
+	[SerializeField] [Min(0)] private float SwitchInteractionRange = 1.5f;
+
 	private void OnEnable() => MouseHoverMonitor.Inst.OnMouseClickGameObject.AddListener(OnClick);
 	private void OnDisable() => MouseHoverMonitor.Inst?.OnMouseClickGameObject.RemoveListener(OnClick);
 
 	private void OnClick(GameObject GO)
 	{
-		if (GO.TryGetComponent(out SwitchTrigger SwTrg))
+		if (Vector3.Distance(GO.transform.position, transform.position) <= SwitchInteractionRange
+			&& GO.TryGetComponent(out SwitchTrigger SwTrgr))
 		{
-			SwTrg.OnClick();
+			SwTrgr.OnClick();
 		}
+	}
+
+	private void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.cyan * 0.5f;
+		Gizmos.DrawWireSphere(transform.position, SwitchInteractionRange);
 	}
 }
